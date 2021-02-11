d674d98288f0e4c11f4277356c0ddbb65bb2e3cd
Cecilia Cerna <cecilia_cer@yahoo.com>
2021-02-11 16:26:19 +0100

    add sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get 'home', to:'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'about', to: 'pages#about'
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
