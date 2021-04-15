ae9a3b66dbfb530ec166e6304d01d96ce7bce574
Cecilia Cerna <cecilia_cer@yahoo.com>
2021-04-15 16:05:15 +0200

    Small changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,4 @@
 class SessionsController < ApplicationController
-
   def new
   end
 
@@ -7,18 +6,17 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      flash[:notice] = " Logged in successfully"
+      flash[:notice] = 'Logged in successfully'
       redirect_to user
     else
-      flash.now[:alert] = "There was something wrong with your login details"
-      render "new"
+      flash.now[:alert] = 'There was something wrong with your login details'
+      render 'new'
     end
   end
 
   def destroy
     session[:user_id] = nil
-    flash[:notice] = "Logged out"
+    flash[:notice] = 'Logged out'
     redirect_to root_path
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :articles
   root 'pages#home'
-  get 'home', to:'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'home', to: 'pages#home'
   get 'about', to: 'pages#about'
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
